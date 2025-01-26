7215fdcf837ae0b99ea6edf8a7e1a928b2f0502f
rasberry <rasberry@hotmail.com>
2025-01-26 13:51:32 -0700

    Fixed protection level on IProgressWithLabel

--- src/IProgressWithLabel.cs
+++ src/IProgressWithLabel.cs
@@ -6,7 +6,7 @@ namespace Rasberry.Cli;
 /// Defines a provider for progress updates along with a label
 /// </summary>
 /// <typeparam name="T">The type of progress update value.</typeparam>
-interface IProgressWithLabel<T> : IProgress<T>
+public interface IProgressWithLabel<T> : IProgress<T>
 {
 	///<summary>Include a label for use with the progress bar</summary>
 	string Label { get; set; }
